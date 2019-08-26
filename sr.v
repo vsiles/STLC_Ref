d635a758453d2b6d96dc9168f104d00317295d2b
Vincent Siles <vsiles@fb.com>
2019-08-26 16:59:41 +0200

    trying to add some subtyping relation

--- sr.v
+++ sr.v
@@ -1,6 +1,104 @@
 Require Import List Arith Omega.
 Require Import STLC_Ref.term STLC_Ref.ty STLC_Ref.env STLC_Ref.red.
 
+Inductive sub: Ty -> Ty -> Prop :=
+ | subArr : forall A B U V, sub U A -> sub B V -> sub (A ⇒ B) (U ⇒ V)
+ | subRefl: forall S, sub S S
+         (* Really not sure about that one, need to think about it *)
+ (* | subRef: forall S T, sub T S -> sub (Ref S) (Ref T) *)
+ | subTrans: forall S T U, sub S T -> sub T U -> sub S U
+.
+
+Lemma inv_arr_l: forall A B T, sub (A ⇒ B) T -> exists U, exists V,
+    T = U ⇒ V /\ sub U A /\ sub B V.
+Proof.
+intros A B T hsub.
+remember (A ⇒ B) as AB.
+revert A B HeqAB.
+induction hsub as [ ? ? ? ? h1 hi1 h2 hi2 | ? (* | ? ? h hi *) | ? ? ? h1 hi1 h2 hi2];
+        intros X Y heq.
+- injection heq; intros; subst; clear heq.
+  exists U; exists V; now split.
+- subst.
+  exists X; exists Y; now repeat split; apply subRefl.
+(* - discriminate. *)
+- subst.
+  destruct (hi1 _ _ (eq_refl _)) as [A [B [hA [? ?]]]].
+  apply hi2 in hA as [W [Z [ hW [? ?]]]].
+  exists W; exists Z; repeat split; [
+     assumption |
+     now apply subTrans with (T := A) |
+     now apply subTrans with (T := B) ].
+Qed.
+
+Lemma inv_arr_r: forall A B S, sub S (A ⇒ B) -> exists U, exists V,
+    S = U ⇒ V /\ sub A U /\ sub V B.
+Proof.
+intros A B S hsub.
+remember (A ⇒ B) as AB.
+revert A B HeqAB.
+induction hsub as [ ? ? ? ? h1 hi1 h2 hi2 | ? (* | ? ? h hi *) | ? ? ? h1 hi1 h2 hi2];
+        intros X Y heq.
+- injection heq; intros; subst; clear heq.
+  exists A; exists B; now split.
+- subst.
+  exists X; exists Y; now repeat split; apply subRefl.
+(* - discriminate. *)
+- subst.
+  destruct (hi2 _ _ (eq_refl _)) as [A [B [hA [? ?]]]].
+  apply hi1 in hA as [W [Z [ hW [? ?]]]].
+  exists W; exists Z; repeat split; [
+     assumption |
+     now apply subTrans with (T := A) |
+     now apply subTrans with (T := B) ].
+Qed.
+
+(* Lemma inv_Ref: forall A T, sub (Ref A) T -> exists U, T = Ref U /\ sub U A. *)
+(* Proof. *)
+(* intros A T hsub. *)
+(* remember (Ref A) as RA. *)
+(* revert A HeqRA. *)
+(* induction hsub as [ ? ? ? ? h1 hi1 h2 hi2 | ? | ? ? h hi | ? ? ? h1 hi1 h2 hi2]; *)
+(*         intros X heq. *)
+(* - discriminate. *)
+(* - subst. *)
+(*   exists X; now split; [ | apply subRefl]. *)
+(* - injection heq; intros; subst; clear heq. *)
+(*   exists T; now split. *)
+(* - subst. *)
+(*   destruct (hi1 _ (eq_refl _)) as [A [hA ?]]. *)
+(*   apply hi2 in hA as [B [hB ?]]. *)
+(*   exists B; split; [ *)
+(*      assumption | *)
+(*      now apply subTrans with (T := A) ]. *)
+(* Qed. *)
+
+Lemma inv_Ref_r_ : forall S T, sub S T -> forall A, T = Ref A -> S = Ref A.
+Proof.
+induction 1 as [ | ? | ? ? ? h1 hi1 h2 hi2]; intros X heq; 
+        [ discriminate | assumption | ].
+apply hi1.
+now apply hi2.
+Qed.
+
+Lemma inv_Ref_l_ : forall S T, sub S T -> forall A, S = Ref A -> T = Ref A.
+Proof.
+induction 1 as [ | ? | ? ? ? h1 hi1 h2 hi2]; intros X heq; 
+        [ discriminate | assumption | ].
+apply hi2.
+now apply hi1.
+Qed.
+
+Lemma inv_Ref_l: forall A T, sub (Ref A) T -> T = Ref A.
+Proof.
+now intros; apply inv_Ref_l_ with (S := (Ref A)).
+Qed.
+
+Lemma inv_Ref_r: forall A S, sub S (Ref A) -> S = Ref A.
+Proof.
+now intros; apply inv_Ref_r_ with (T := (Ref A)).
+Qed.
+
 (* This map is used as truth source to type memories: each address in the
    the map is associated with a typ. The idea is that memories should be
    typed in the empty context, so we get something like:
@@ -48,6 +146,7 @@ Inductive typ : Env -> TStore -> Term -> Ty -> Prop :=
  | cAsg  : forall Γ D M N A, typ Γ D M (Ref A) -> typ Γ D N A ->
          typ Γ D (M :=: N) Unit 
  | cDrf  : forall Γ D M A, typ Γ D M (Ref A) -> typ Γ D (deref M)  A
+ | cSub  : forall Γ D M A B, sub A B -> typ Γ D M A -> typ Γ D M B
 .
 
 Hint Constructors typ.
@@ -56,12 +155,14 @@ Lemma InclTStore_typ: forall Γ D1 M T, typ Γ D1 M T -> forall D2,
     InclTStore D1 D2 -> typ Γ D2 M T.
 Proof.
 induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi ]; intros  D2 hincl; try solve [
+        r ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
+        ? ? ? ? h hi | ? ? ? ? ? hsub h hi ]; intros  D2 hincl; try solve [
             constructor; now apply hi | now constructor
             | econstructor; [now apply hi1 | now apply hi2 ] ].
-constructor.
-now apply hincl.
+- constructor.
+  now apply hincl.
+- apply cSub with (A := A); [ now apply hsub | ].
+  now apply hi.
 Qed.
 
 (* a type store D types a memory m if they map the same set of addresses
@@ -79,7 +180,8 @@ Theorem weakening: forall Δ D M T, typ Δ D M T ->
     forall Γ A n Δ', ins_in_env Γ A n Δ Δ' -> typ Δ' D (M ↑ 1 # n) T.
 Proof.
 induction 1 as [ Δ D ? ? hin | Δ D ? ? ? h hi | Δ D ? ? ? ? h1 hi1 h2 hi2 |
-        Δ D ? ? h hi | Δ D | Δ D ? ? ? | Δ D ? ? ? h1 hi1 h2 hi2 | Δ D ? ? h hi ];
+        Δ D ? ? h hi | Δ D | Δ D ? ? ? | Δ D ? ? ? h1 hi1 h2 hi2 |
+                Δ D ? ? h hi | Δ D ? ? ? hsub h hi ];
         intros Γ ? n Δ' hins; simpl in *; try solve [
             constructor; eapply hi; constructor; now apply hins |
             constructor; eapply hi; now apply hins |
@@ -89,6 +191,9 @@ induction 1 as [ Δ D ? ? hin | Δ D ? ? ? h hi | Δ D ? ? ? ? h1 hi1 h2 hi2 |
   + eapply ins_item_ge; [ now apply hins | assumption | assumption].
   + eapply ins_item_lt; [ now apply hins | | ]; assumption.
 - now constructor.
+- econstructor; [ now apply hsub | ].
+  eapply hi.
+  now apply hins.
 Qed.
 
 Theorem thinning :
@@ -129,7 +234,8 @@ Theorem substitution : forall Γ D M T , typ Γ D M T  ->
  forall Γ' n , sub_in_env Δ A n Γ Γ' -> typ Γ' D (M [ n ←P ])  T.
 Proof.
 induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 | ? ? ? ? h hi ];
+        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 |
+                ? ? ? ? h hi | ? ? ? ? ? hs h hi ];
     intros Δ P X hP Γ' n hsub; simpl; try solve [
         now econstructor; [eapply hi1; [apply hP | ] |
                 eapply hi2; [apply hP | ]]
@@ -151,6 +257,9 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
   eapply hi; [ now apply hP | ].
   now constructor.
 - now constructor.
+- econstructor; [ now apply hs | eapply hi].
+  + now apply hP.
+  + assumption.
 Qed.
 
 (* Adding a fresh variable to a type store doesn't change existing typing 
@@ -160,15 +269,18 @@ Lemma bindAddr_thinning: forall Γ D M T, typ Γ D M T -> forall u S,
     ~ InAddr u D -> typ Γ (bindAddr u S D) M T.
 Proof.
 induction 1 as [ ? ? ? v ? | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 | ? ? ? ? h hi ];
+        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 | ? ? ? ? h hi
+        | ? ? ? ? ? hsub h hi ];
         intros w S hnotin; try solve [
             now constructor |
             now constructor; apply hi |
             now econstructor; [ apply hi1 | apply hi2 ]].
-constructor; simpl.
-destruct Nat.eq_dec; [subst | assumption].
-elim hnotin.
-now apply read2In with T.
+- constructor; simpl.
+  destruct Nat.eq_dec; [subst | assumption].
+  elim hnotin.
+  now apply read2In with T.
+- econstructor; [ now apply hsub | ].
+  now apply hi.
 Qed.
 
 (* Adding a duplicate address with the same type that is already bound in
@@ -178,14 +290,17 @@ Lemma bindAddr_thinning_same: forall Γ D M T, typ Γ D M T -> forall u S,
     readAddr u D = Some S -> typ Γ (bindAddr u S D) M T.
 Proof.
 induction 1 as [ ? ? ? v ? | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 | ? ? ? ? h hi ];
+        ? ? ? ? h hi | ? ? | ? ? ? ? | ? ? ? ? ? h1 hi1 h2 hi2 |
+                ? ? ? ? h hi | ? ? ? ? ? hsub h hi ];
         intros w S hr; try solve [
             now constructor |
             now constructor; apply hi |
             now econstructor; [ apply hi1 | apply hi2 ]].
-constructor; simpl.
-destruct Nat.eq_dec; [subst | assumption].
-now elim hr.
+- constructor; simpl.
+  destruct Nat.eq_dec; [subst | assumption].
+  now elim hr.
+- econstructor; [ now apply hsub | ].
+  now apply hi.
 Qed.
   
 Lemma bindWfTStore: forall D mem u V A,
@@ -209,6 +324,68 @@ intros D mem u V A [h1 h2] htyp hin; split.
     intro hh; apply hin; now apply h1.
 Qed.
 
+(* Inversion Lemmas *)
+Lemma inv_la: forall Γ D A M T, typ Γ D (λ[A], M) T -> exists B,
+    sub (A ⇒  B) T /\ typ (A:: Γ) D M B.
+Proof.
+intros Γ D A M T; remember (λ[A], M) as L; intro typ; revert A M HeqL.
+induction typ; intros; try discriminate.
+- injection HeqL; intros; subst; clear HeqL.
+  now exists B; split; [now apply subRefl | ].
+- subst.
+  destruct (IHtyp _ _ (eq_refl _)) as [ B0 [h1 h2]].
+  exists B0; split; [ | assumption ].
+  now apply subTrans with (T := A).
+Qed.
+
+Lemma inv_var : forall Γ D x A, typ Γ D #x A -> exists B, sub B A /\ B ↓ x ∈ Γ. 
+Proof.
+intros Γ D x A htyp; remember #x as X; revert x HeqX.
+induction htyp; intros; try discriminate.
+- injection HeqX; intros; subst; clear HeqX.
+  exists A; intuition.
+  now apply subRefl.
+- destruct (IHhtyp x HeqX) as (A' & ? & ?); exists A'; split; [ | assumption ].
+  now apply subTrans with (T := A).
+Qed.
+
+Lemma inv_app : forall Γ D M N T, typ Γ D (M • N) T -> exists A, exists B,
+    sub B T /\ typ Γ D M (A ⇒  B) /\ typ Γ D N A.
+Proof.
+intros Γ D M N T htyp; remember (M • N) as MN; revert M N HeqMN.
+induction htyp; intros; try discriminate.
+- clear IHhtyp1 IHhtyp2. injection HeqMN; intros; subst; clear HeqMN.
+  exists A; exists B; intuition.
+  now apply subRefl.
+- destruct (IHhtyp M0 N HeqMN) as (K & L & ? & ?& ?); exists K; exists L;
+  split; [ now apply subTrans with (T := A) | ].
+  split; assumption.
+Qed.
+
+Lemma inv_addr: forall Γ D u T, typ Γ D (Addr u) T -> exists U,
+    sub (Ref U) T /\ readAddr u D = Some U.
+Proof.
+intros Γ D u T htyp; remember (Addr u) as AU; revert u HeqAU.
+induction htyp; intros; try discriminate.
+- injection HeqAU; intros; subst; clear HeqAU.
+  exists T; now split; [ now apply subRefl | assumption ].
+- subst; destruct (IHhtyp _ (eq_refl _)) as [ U [h1 h2]].
+  exists U; split; [ now apply subTrans with (T := A) | ].
+  assumption.
+Qed.
+
+Lemma inv_ref: forall Γ D M T, typ Γ D (ref M) T -> exists U, sub (Ref U) T /\ typ Γ D M U.
+Proof.
+intros Γ D M T htyp; remember (ref M) as RM; revert M HeqRM.
+induction htyp; intros; try discriminate.
+- clear IHhtyp.
+  injection HeqRM; intros; subst; clear HeqRM.
+  exists A; now split; [ now apply subRefl | assumption ].
+- subst; destruct (IHhtyp _ (eq_refl _)) as [ U [h1 h2]].
+  exists U; split; [ now apply subTrans with (T := A) | ].
+  assumption.
+Qed.
+
 (* Subject Reduction *)
 (* TODO: clean a bit the script *)
 Lemma SR: forall Γ D M T, typ Γ D M T -> forall m1 m2 N,
@@ -217,15 +394,20 @@ Lemma SR: forall Γ D M T, typ Γ D M T -> forall m1 m2 N,
 Proof.
 induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
         ? ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi ];
+        ? ? ? ? h hi | ? ? ? ? ? hsbu h hi];
           intros m1 m2 Q hgamma hred hst; try solve [
             inversion hred; subst; clear hred; constructor; now apply hi
             ].
 - inversion hred; subst; clear hred.
   + exists D; split; [ now apply InclTStore_refl |].
     split; [assumption |].
-    inversion h1; subst; clear h1.
-    eapply substitution; [ now apply H2 | now apply h2 | now constructor ].
+    apply inv_la in h1 as [B0 [hinv1 hinv2]].
+    apply inv_arr_l in hinv1 as [ U[ V[ heq[ hU hV]]]].
+    injection heq; intros; subst; clear heq.
+    eapply substitution.
+    * eapply cSub; [ now apply hV | now apply hinv2 ].
+    * eapply cSub; [ now apply hU | now apply h2 ].
+    * now constructor. 
   + destruct (hi1 m1 m2 M'  (refl_equal _) H4 hst) as [Q [hQ1 [hQ2 hQ3]]].
     exists Q; split; [assumption | ].
     split; [assumption |].
@@ -268,11 +450,13 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
     split; [assumption |].
     now constructor.
 - inversion hred; subst; clear hred.
-  + inversion h1; subst; clear h1.
-    exists (bindAddr u A D); repeat split; try assumption.
+  + apply inv_addr in h1 as [U [hinv1 hinv2]].
+    apply inv_Ref_l in hinv1.
+    injection hinv1; intros; subst; clear hinv1.
+    exists (bindAddr u U D); repeat split; try assumption.
     * intros x X h; simpl; rewrite h.
       destruct Nat.eq_dec; [ | now idtac].
-      now subst; rewrite H3 in h.
+      now subst; rewrite hinv2 in h.
     * simpl.
       destruct Nat.eq_dec as [heq | hne]; [now idtac | intro hh].
       now apply hst.
@@ -300,7 +484,9 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
     econstructor; [ | now apply hQ3 ].
     now apply InclTStore_typ with D.
 - inversion hred; subst; clear hred.
-  + inversion h; subst; clear h.
+  + apply inv_addr in h as [U [hU ?]].
+    apply inv_Ref_l in hU.
+    injection hU; intros; subst; clear hU.
     exists D; repeat split; [ now apply InclTStore_refl | | | | ].
     * now intro hh; apply hst.
     * now intro hh; apply hst.
@@ -311,7 +497,7 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
       now destruct (readAddr w D).
     * destruct hst as [h1 h2].
       apply h2 in H0.
-      now rewrite H4 in H0.
+      now rewrite H in H0.
   + destruct (hi m1 m2 M' (refl_equal _) H0 hst) as [Q [h1 [h2 h3]]].
     exists Q; repeat split; try assumption.
     * now intro hh; apply h2.
@@ -319,6 +505,45 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
     * intros w V hw.
       now apply h2 in hw.
     * now constructor.
+- destruct (hi _ _ _ hgamma hred hst) as [DD [ h1 [h2 h3]]].
+  exists DD; split; [assumption | split; [assumption | ]].
+  now apply cSub with (A := A).
+Qed.
+
+Lemma Progress_sub_la: forall Γ D M T, typ Γ D M T -> forall A B,
+    T = A ⇒ B -> is_value M -> Γ = nil -> exists U, exists K,
+    M = λ[U], K /\ typ (U::Γ) D K B /\ sub A U.
+Proof.
+induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
+        ? ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
+        ? ? ? ? h hi | ? ? ? ? hsub h hi]; intros X Y heq hV hG; subst;
+                try discriminate; try elim hV.
+- now inversion hin.
+- injection heq; intros; subst; clear heq hV.
+  exists X; exists M; split; [reflexivity | split; [assumption | ]].
+  now apply subRefl.
+- apply inv_arr_r in h as [U [V [-> [hinvU hinvV]]]].
+  destruct (IHhi _ _ (eq_refl _) hV (eq_refl _)) as
+     [ Z [ K [ -> [htyp hsub]]]].
+  exists Z; exists K; split;[ reflexivity | split].
+  + now apply cSub with (A := V).
+  + now apply subTrans with (T := U).
+Qed.
+
+Lemma Progress_sub_addr: forall Γ D M T, typ Γ D M T -> forall A,
+    T = Ref A -> is_value M -> Γ = nil -> exists u,
+    M = Addr u /\ readAddr u D = Some A.
+Proof.
+induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
+        ? ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
+        ? ? ? ? h hi | ? ? ? ? hsub h hi]; intros X heq hV hG; subst;
+                try discriminate; try elim hV.
+- now inversion hin.
+- injection heq; intros; subst; clear heq hV.
+  exists u; split; [reflexivity | assumption].
+- apply inv_Ref_r in h; subst.
+  destruct (IHhi _ (eq_refl _) hV (eq_refl _)) as [ u [ -> h]].
+  now exists u; split; [reflexivity | ].
 Qed.
 
 Lemma Progress_: forall Γ D M T, typ Γ D M T -> Γ = nil ->
@@ -327,14 +552,15 @@ Lemma Progress_: forall Γ D M T, typ Γ D M T -> Γ = nil ->
 Proof.
 induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
         ? ? ? ? h hi | ? ? | ? ? ? ? h | ? ? ? ? ? h1 hi1 h2 hi2 |
-        ? ? ? ? h hi ]; intros hgamma m1 hst; try (now right).
+        ? ? ? ? h hi | ? ? ? ? hsub h hi]; intros hgamma m1 hst; try (now right).
 - left.
   destruct (hi1 hgamma m1 hst) as [[M' [m2 hred]] | hV].
   + exists (M' • N); exists m2; now constructor.
   + destruct (hi2 hgamma m1 hst) as [[N' [m2 hred]] | hW].
     * exists (M • N'); exists m2; now constructor.
-    * inversion h1; subst; clear h1; try now elim hV.
-      now exists (M0 [← N]); exists m1; constructor.
+    * destruct (Progress_sub_la _ _ _ _ h1 A B (eq_refl _) hV hgamma) as
+      [ U [ K [ -> [ hU htyp]]]].
+      now exists (K [← N]); exists m1; constructor.
 - left.
   destruct (hi hgamma m1 hst) as [[M' [m2 hred]] | hV].
   + exists (ref M'); exists m2; now constructor.
@@ -346,18 +572,23 @@ induction 1 as [ ? ? ? v hin | ? ? ? ? ? h hi | ? ? ? ? ? ? h1 hi1 h2 hi2 |
   + exists (M' :=: N); exists m2; now constructor.
   + destruct (hi2 hgamma m1 hst) as [[N' [m2 hred]] | hW].
     * exists (M :=: N'); exists m2; now constructor.
-    * inversion h1; subst; clear h1; try now elim hV.
+    * apply Progress_sub_addr with (A := A) in h1 as [u [-> hu]];
+      [ | reflexivity | assumption | assumption ].
       now exists unit; exists (bindAddr u N m1); constructor.
 - left. 
   destruct (hi hgamma m1 hst) as [[M' [m2 hred]] | hV].
   + exists (deref M'); exists m2; now constructor.
-  + inversion h; subst; clear hi; try now elim hV.
+  + apply Progress_sub_addr with (A := A) in h as [u [-> hu]];
+      [ | reflexivity | assumption | assumption ].
     case_eq (readAddr u m1).
     * now intros V heq; exists V; exists m1; constructor.
-    * intro hr; apply read2In in H3.
-      apply hst in H3.
-      apply In2read in H3 as [ ? hh].
+    * intro hr; apply read2In in hu.
+      apply hst in hu.
+      apply In2read in hu as [ ? hh].
       now rewrite hh in hr; discriminate.
+- destruct (IHhi hgamma m1 hst) as [[N [m2 hred]] | hV].
+  + now left; exists N; exists m2.
+  + now right.
 Qed.
 
 Lemma Progress: forall D M T, typ nil D M T -> 
